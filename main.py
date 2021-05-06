a0507c9a02a4685a7dc3bdf5de87ec0941dbfda4
haduchau321 <83698436+haduchau321@users.noreply.github.com>
2021-05-06 18:04:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 from fastapi import FastAPI
-#  import uvicorn
-from pydantic import BaseModel
-
-class myghi(BaseModel):
-    name:str
-    passwrod:str
-    trangthai:str = False
 
 app = FastAPI()
 
-@app.get('/')
-async def home():
-    data = {'name':'haduchau','password':'yeutaodi123'}
-    return data
-
-@app.post('/dangki')
-async def submit(out:myghi):
-    name = out.name
-    passwrod = out.passwrod
-    trangthai = out.trangthai
-    print(name,passwrod,trangthai)
-    return out,True
+#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
-# if __name__ == '__main__':
-#     uvicorn.run(app)
+@app.get("/")
+def home():
+    return {"message":"Hello TutLinks.com"}
